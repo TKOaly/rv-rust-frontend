c0ee422a6d4540908fb36c75b0052d351f276164
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2024-12-13 14:54:49 +0200

    Use hard-coded timezone
    RV Location timezone deemed unlikely to change

--- src/user_loop.rs
+++ src/user_loop.rs
@@ -1612,7 +1612,7 @@ pub fn user_loop(credentials: &rv_api::AuthenticationResponse, terminal_io: &mut
                                         "{}{}{} {}€",
                                         DateTime::parse_from_rfc3339(&event.time)
                                             .unwrap()
-                                            .with_timezone(&chrono::offset::Local)
+                                            .with_timezone(&chrono_tz::Europe::Helsinki)
                                             .format("%d/%m/%Y %H:%M"),
                                         if event.returned {
                                             " bought [returned] "
